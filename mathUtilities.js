c5925c33fa8e101d122a198ba630da0da4e0fc59
cogi234 <colinbougie@gmail.com>
2024-09-26 10:31:11 -0400

    Create mathUtilities.js

---
+++ mathUtilities.js
@@ -0,0 +1,25 @@
+export function factorial(n) {
+    if (n === 0 || n === 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+export function isPrime(n) {
+    let m = Math.sqrt(n);
+    for (let i = 2; i <= m; i++)
+        if (n % i === 0)
+            return false;
+
+    return value > 1;
+}
+
+export function findPrime(n) {
+    let primeNumber = 0;
+    for (let i = 0; i < n; i++) {
+        primeNumber++;
+        while (!isPrime(primeNumber))
+            primeNumber++;
+    }
+
+    return primeNumber;
+}
